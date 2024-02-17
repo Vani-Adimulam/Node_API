9c2d39991c2b888b600ed48a2fdc8d7114893d24
LAPTOP-4VM21UJB\vania <testrepositorybitbucket2@gmail.coom>
2024-02-17 18:08:29 +0530

    Node commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => [
+    res.send("Helllo node api")
+])
+app.listen(3003, ()=> {
+    console.log(`Port is running on 3003`)
+})
